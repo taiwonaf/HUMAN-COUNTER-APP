70b70c631b5258eb258267967df688fe204d74fd
taiwonaf <nafiutaiwosolomon@gmail.com>
2022-05-17 23:58:02 +0100

    Reset button updated, button interaction included

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let countEl = document.getElementById("count-el");
 let saveEl = document.getElementById("save-el");
+let resetBtn = document.getElementById("reset-btn");
 
 let count = 0;
+
+resetBtn.style.display = "none"
 
 function counter() {
    count += 1
@@ -12,8 +15,11 @@ function save() {
     saveEl.textContent += `${count} - `
     count = 0
     countEl.textContent = 0
+    resetBtn.style.display = "inline-block"
 }
+
 
 function reset() {
     saveEl.textContent = `Previous count: `
+    resetBtn.style.display = "none"
 }
